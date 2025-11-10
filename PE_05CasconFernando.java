b0203ff5aee79f39ec416e28a75db03d7af9e161
Fernando <nandocascon24@gmail.com>
2025-11-10 20:40:18 +0100

    mejoras

--- PE_05CasconFernando.java
+++ PE_05CasconFernando.java
@@ -2,7 +2,6 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 public class PE_05CasconFernando {
     Scanner sc = new Scanner(System.in);
-    int total = 0;
     public static void main(String[] args) {
         PE_05CasconFernando p = new PE_05CasconFernando();
         p.principal();
@@ -11,7 +10,9 @@ public class PE_05CasconFernando {
     public void principal() {
         int mainMenu = 0;
         String ticket = "______________________________________\n============ NOVA COMANDA ============\n______________________________________\n";
-        String client = "";
+        String client = "", producte = "", afegir = "";
+        double preuUnitari = 0, subtotal = 0, total = 0;
+        int quantitat = 0;
 
         do {
             System.out.println("______________________________________");
@@ -32,78 +33,109 @@ public class PE_05CasconFernando {
                     System.out.print("Introdueix el nom del client: ");
                     client = sc.nextLine();
 
-                    ticket = (ticket + "Client: " + client + "\nProducte\tQuantitat\tPreu unit.\tSubtotal\n");
-                    ticket = (ticket + "---------------------------------------------------------\n");
+                    ticket += ("Client: " + client + "\nProducte\tQuantitat\tPreu unit.\tSubtotal\n---------------------------------------------------------\n");
                     
-                    ticket = calcularPrecio(ticket);
+                    do {
+                        producte = producte();
+                        preuUnitari = preuUnitari();
+                        quantitat = quantitat();
+                        afegir = afegir(); 
+
+                        subtotal = preuUnitari * quantitat;
+                        total += subtotal;
+
+                        ticket += String.format("%-15s %-15d %-15s %s\n", producte, quantitat, String.format("%.2f€", preuUnitari), String.format("%.2f€", subtotal));
+
+                    } while (afegir.equalsIgnoreCase("s"));
+
+                    ticket = calculIva(total, ticket);
+
                     System.out.println(ticket);
                     break;
             }
-
         } while (mainMenu != 4);
-      
 
         sc.close();
     }
 
-    public String LlegirDades(String ticket) {
-        String producte = "", altreProducte = "";
-        int quantitat = 0;
-        double preuUnitari = 0, subtotal = 0;
+    public String producte() {
+        String producte = "";
+        System.out.print("Introdueix el producte: ");
+        producte = sc.nextLine();
+
+        return producte;
+    }
+
+    public double preuUnitari() {
+        double preuUnitari = 0;
         boolean validation = true;
 
         do {
-            System.out.print("Introdueix el producte: ");
-            producte = sc.nextLine();
+            try {
+                System.out.print("Preu unitari: ");
+                preuUnitari = sc.nextDouble();
+                validation = false;
 
-            do {
-                try {
-                    System.out.print("Preu unitari: ");
-                    preuUnitari = sc.nextDouble();
-                    validation = false;
-
-                } catch (InputMismatchException e) {
-                    System.out.println("Format invàlid.");
-                    sc.nextLine();
-                }
-            } while (validation);
-                
-            validation = true;
-
-            do {
-                try {
-                    System.out.print("Quantitat: ");
-                    quantitat = sc.nextInt();
-                    sc.nextLine();
-                    validation = false;
-
-                } catch (InputMismatchException e) {
-                    System.out.println("Format invàlid.");
-                    sc.nextLine();
-                }
-            } while (validation);                
-
-            System.out.print("Vols afegir un altre producte? (s/n): ");
-            altreProducte = sc.nextLine();         
-
-            subtotal = preuUnitari * quantitat;
-            ticket += String.format("%-15s %-15d %-15s %s\n", producte, quantitat, String.format("%.2f€", preuUnitari), String.format("%.2f€", subtotal));
-
-            total += subtotal;
-
-        } while (altreProducte.equalsIgnoreCase("s"));
-        
-        return ticket;
+            } catch (InputMismatchException e) {
+                System.out.println("Format invàlid.");
+                sc.nextLine();
+            }
+        } while (validation);
+        return preuUnitari;
     }
 
-    public String calcularPrecio(String ticket){
-        ticket = LlegirDades(ticket);
+    public int quantitat() {
+        int quantitat = 0;
+        boolean validation = true;
+
+        do {
+            try {
+                System.out.print("Quantitat: ");
+                quantitat = sc.nextInt();
+                sc.nextLine();
+                validation = false;
+
+            } catch (InputMismatchException e) {
+                System.out.println("Format invàlid.");
+                sc.nextLine();
+            }
+        } while (validation);
+        return quantitat;
+    }
+
+    public String afegir() {
+        String afegir = "";
+        boolean validation = true;
+
+        do {
+            System.out.print("Vols afegir un altre producte? (s/n): ");
+            afegir = sc.nextLine(); 
+
+            if (afegir.equalsIgnoreCase("s") || afegir.equalsIgnoreCase("n")) {
+                validation = false;
+                
+            } else {
+                System.out.println("Error, insereix una opció vàlida.");
+            }
+
+        } while (validation == true);
+
+        return afegir;
+    }
+
+    public String calculIva(double total, String ticket) {
         double iva = 0.1;
-        double ivaTotal = 0; 
+        double ivaTotal = 0;
         double preuFinal = 0;
 
-        ivaTotal = total*iva;
+        ivaTotal = total * iva;
         preuFinal = total + ivaTotal;
+
+        ticket += "---------------------------------------------------------\n";
+        ticket += String.format("%-30s %15.2f€\n", "Total sense IVA:", total);
+        ticket += String.format("%-30s %15.2f€\n", "IVA (10%):", ivaTotal);
+        ticket += String.format("%-30s %15.2f€\n", "TOTAL A PAGAR:", preuFinal);
+        ticket += "=========================================================\n";
 
         return ticket;
     }
